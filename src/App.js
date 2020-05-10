0aa21c6ee0fcaa023b2b9762662f40a093e9ea5d
Kaikaine <kairijb@protonmail.com>
2020-05-10 18:30:20 -0500

    subscribe to auth state changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
@@ -6,17 +6,37 @@ import ShopPage from "./pages/ShopPage";
 import Header from "./components/Header";
 import SignInAndSignUpPage from "./pages/SignInAndSignUpPage";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+import { auth } from "./firebase/firebase";
+
+class App extends Component {
+  state = {
+    currentUser: null,
+  };
+
+  unscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
