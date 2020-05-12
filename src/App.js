05ffe43c24760eb91ab7a54a3103cb1e6285800c
Kaikaine <kairijb@protonmail.com>
2020-05-11 19:31:54 -0500

    sign in and sign up with email and password

--- src/App.js
+++ src/App.js
@@ -16,9 +16,21 @@ class App extends Component {
   unscribeFromAuth = null;
 
   componentDidMount() {
-    this.unscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
